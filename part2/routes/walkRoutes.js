3214a87d4cd1d2a9f995296ebef85ccdb12a8669
TommyWUTAO <tommytao169@gmail.com>
2025-06-20 05:43:31 +0000

    [automatic commit] 2025-06-20 05:43:31 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'pending'
+      WHERE wr.status = 'open'
     `);
     res.json(rows);
   } catch (error) {
